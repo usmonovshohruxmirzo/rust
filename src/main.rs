5a96d3d7c5bfd3c3eccc6848c4ae3c93e5ca9cab
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-10-13 23:53:47 +0500

    feat: add simple CLI program that greets the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    let name = name.trim();
+
+    println!("Hello, {}!", name);
 }
